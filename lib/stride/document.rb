d45ab0a3b26d35d0f097a21b72ea943cacb05925
Jon Evans <joncodes+github@gmail.com>
2017-10-13 11:29:26 -0600

    Convert `:emoji:`s to json

--- lib/stride/document.rb
+++ lib/stride/document.rb
@@ -27,7 +27,7 @@ module Stride
     attr_accessor :html
 
     def content_blocks
-      nokogiri_elements.map { |element| ElementJson.new(element).to_s }
+      nokogiri_elements.flat_map { |element| ElementJson.new(element).as_json }
     end
 
     def nokogiri_elements
@@ -44,7 +44,7 @@ module Stride
         self.nokogiri_element = nokogiri_element
       end
 
-      def to_s
+      def as_json
         case nokogiri_element
         when Nokogiri::XML::Text then text_element
         else                          link_element
@@ -56,10 +56,44 @@ module Stride
       attr_accessor :nokogiri_element
 
       def text_element
+        text = nokogiri_element.text
+
+        if emoji?
+          emoji_element
+        elsif contains_emoji?
+          text_and_emoji_elements
+        else
+          {
+            "type": "text",
+            "text": nokogiri_element.text
+          }
+        end
+      end
+
+      def emoji?
+        nokogiri_element.text =~ /\A\:\w+\:\Z/
+      end
+
+      def emoji_element
         {
-          "type": "text",
-          "text": nokogiri_element.text
+          "type": "emoji",
+          "attrs": {
+            "shortName": nokogiri_element.text
+          }
         }
+      end
+
+      def contains_emoji?
+        nokogiri_element.text =~ /\:\w+\:/
+      end
+
+      def text_and_emoji_elements
+        components = nokogiri_element.text.match(/(.*)(\:\w+\:)(.*)/)
+
+        [components[1], components[2], components[3]].select { |string| string != '' }.map do |component|
+          nokogiri_text = Nokogiri::XML::Text.new(component, Nokogiri::XML::Document.new)
+          self.class.new(nokogiri_text).as_json
+        end
       end
 
       def link_element
--- spec/tests/lib/stride/document_spec.rb
+++ spec/tests/lib/stride/document_spec.rb
@@ -2,45 +2,143 @@ require 'spec_helper'
 
 module Stride
   RSpec.describe Document do
-    let(:document) do
-      described_class.new('Hi from <a href="https://bonus.ly">Bonusly</a> team')
-    end
+    let(:document) { described_class.new(html) }
 
     describe '#as_json' do
-      it 'converts html into stride-formatted documents' do
-        expect(document.as_json).to eq(
-          {
-            "version": 1,
-            "type": "doc",
-            "content": [
+      context 'when links are included' do
+        let(:html) { 'Hi from <a href="https://bonus.ly">Bonusly</a> team' }
+
+        it 'converts converts into stride-friendly json' do
+          expect(document.as_json).to eq(
+            {
+              "version": 1,
+              "type": "doc",
+              "content": [
+                {
+                  "type": "paragraph",
+                  "content": [
+                    {
+                      "type": "text",
+                      "text": "Hi from "
+                    },
+                    {
+                      "type": "text",
+                      "text": "Bonusly",
+                      "marks": [
+                        {
+                          "type": "link",
+                          "attrs": {
+                            "href": "https://bonus.ly"
+                          }
+                        }
+                      ]
+                    },
+                    {
+                      "type": "text",
+                      "text": " team"
+                    }
+                  ]
+                }
+              ]
+            }
+          )
+        end
+      end
+
+      context 'when emojis are included' do
+        context 'in the middle' do
+          let(:html) { 'Hi :smiley: team' }
+
+          it 'converts into stride-friendly json' do
+            expect(document.as_json).to eq(
               {
-                "type": "paragraph",
+                "version": 1,
+                "type": "doc",
                 "content": [
                   {
-                    "type": "text",
-                    "text": "Hi from "
-                  },
-                  {
-                    "type": "text",
-                    "text": "Bonusly",
-                    "marks": [
+                    "type": "paragraph",
+                    "content": [
                       {
-                        "type": "link",
+                        "type": "text",
+                        "text": "Hi "
+                      },
+                      {
+                        "type": "emoji",
                         "attrs": {
-                          "href": "https://bonus.ly"
+                          "shortName": ":smiley:"
                         }
+                      },
+                      {
+                        "type": "text",
+                        "text": " team"
                       }
                     ]
-                  },
-                  {
-                    "type": "text",
-                    "text": " team"
                   }
                 ]
               }
-            ]
-          }
-        )
+            )
+          end
+        end
+
+        context 'in the beginning' do
+          let(:html) { ':smiley: Hi' }
+
+          it 'converts into stride-friendly json' do
+            expect(document.as_json).to eq(
+              {
+                "version": 1,
+                "type": "doc",
+                "content": [
+                  {
+                    "type": "paragraph",
+                    "content": [
+                      {
+                        "type": "emoji",
+                        "attrs": {
+                          "shortName": ":smiley:"
+                        }
+                      },
+                      {
+                        "type": "text",
+                        "text": " Hi"
+                      }
+                    ]
+                  }
+                ]
+              }
+            )
+          end
+        end
+
+        context 'at the end' do
+          let(:html) { 'Hi :smiley:' }
+
+          it 'converts into stride-friendly json' do
+            expect(document.as_json).to eq(
+              {
+                "version": 1,
+                "type": "doc",
+                "content": [
+                  {
+                    "type": "paragraph",
+                    "content": [
+                      {
+                        "type": "text",
+                        "text": "Hi "
+                      },
+                      {
+                        "type": "emoji",
+                        "attrs": {
+                          "shortName": ":smiley:"
+                        }
+                      }
+                    ]
+                  }
+                ]
+              }
+            )
+          end
+        end
       end
     end
   end
