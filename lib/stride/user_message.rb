d99d63c7f30e6b0b4fa80ce82464b56abde45d0c
Jon Evans <joncodes+github@gmail.com>
2018-01-19 16:12:57 -0700

    Ensure bot is installed in the direct conversation with the user first

--- lib/stride/user_message.rb
+++ lib/stride/user_message.rb
@@ -8,12 +8,35 @@ module Stride
     end
 
     def send!
+      UserInstallationRequest.new(access_token, cloud_id, user_id).json
       Request.new(access_token, cloud_id, user_id, message_body).json
     end
 
     private
 
     attr_accessor :access_token, :cloud_id, :user_id, :message_body
+
+    class UserInstallationRequest < AuthorizedRequest
+      def initialize(access_token, cloud_id, user_id)
+        self.access_token = access_token
+        self.cloud_id     = cloud_id
+        self.user_id      = user_id
+      end
+
+      private
+
+      attr_accessor :cloud_id, :user_id
+
+      def uri
+        URI(
+          "#{Stride.configuration.api_base_url}/site/#{cloud_id}/conversation/user/#{user_id}"
+        )
+      end
+
+      def request_class
+        Net::HTTP::Get
+      end
+    end
 
     class Request < AuthorizedRequest
       def initialize(access_token, cloud_id, user_id, message_body)
